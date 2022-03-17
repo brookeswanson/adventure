2e8b8e55ccc1250459311af3441334fa797d2f44
bswanson <brookeswanson09@gmail.com>
2022-03-16 22:34:04 -0700

    Add a single test and a index page.

--- project.clj
+++ project.clj
@@ -3,5 +3,13 @@
   :url ""
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns adventure.core})
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [compojure "1.6.2"]
+                 [ring/ring-defaults "0.3.3"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler adventure.core/app
+         :port 8080}
+  :min-lein-version "2.0.0"
+  :repl-options {:init-ns adventure.core}
+  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "4.0.1"]
+                                  [ring/ring-mock "0.4.0"]]}})
--- test/adventure/core_test.clj
+++ test/adventure/core_test.clj
@@ -1,3 +1,10 @@
 (ns adventure.core-test
-  (:require [clojure.test :refer :all]
-            [adventure.core :refer :all]))
+  (:require
+   [ring.mock.request :as mock]
+   [clojure.test :refer [deftest testing is]]
+   [adventure.core :as core]))
+
+(deftest sms-handler-test
+  (testing "Given any request object return a 200"
+    (let [{:keys [status body]} (core/sms-handler {})]
+      (is (= 200 status)))))
