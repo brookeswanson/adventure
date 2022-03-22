a1a8bd3f463e81a95df66eb72b3afcb256b3bec2
bswanson <brookeswanson09@gmail.com>
2022-03-21 23:28:20 -0700

    Use ragtime for migrations
    * create game table
    * run migrations on server start
    * pull in outpace config to pull db spec.

--- project.clj
+++ project.clj
@@ -4,11 +4,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[compojure "1.6.2"]
+                 [com.outpace/config "0.13.5"]
                  [com.twilio.sdk/twilio "8.27.1"]
+                 [dev.weavejester/ragtime "0.9.1"]
                  [org.clojure/clojure "1.10.3"]
+                 [org.postgresql/postgresql "42.3.3"]
                  [ring/ring-defaults "0.3.3"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler adventure.core/app
+         :init adventure.db.migrations/migrate
          :port 8080}
   :min-lein-version "2.0.0"
   :repl-options {:init-ns adventure.core}
